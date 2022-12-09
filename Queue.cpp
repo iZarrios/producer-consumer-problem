62203acdf953b3e5600bfc7f0abb77b17890cb41
iZarrios <zarriose@gmail.com>
2022-12-10 01:45:03 +0200

    Add custom Queue

--- Queue.cpp
+++ Queue.cpp
@@ -1,7 +1,78 @@
-#include "custom_queue.h"
+#include "Queue.h"
 
+#include <bits/stdc++.h>
 
+using namespace std;
 
+Queue::Queue()
+{
+    data = new Commidity *[MAX_SIZE];
+    /* for (int i = 0; i < MAX_SIZE; i++) */
+    /* { */
+    /*     data[i] = nullptr; */
+    /* } */
 
+    head = -1;
+    tail = 0;
+    size = 0;
+}
 
+int Queue::getSize()
+{
+    return size;
+}
 
+bool Queue::isEmpty()
+{
+    return size == 0;
+}
+
+Commidity *Queue::front()
+{
+    if (head == -1)
+    {
+        cout << "Queue is empty " << endl;
+        return 0;
+    }
+    return data[head];
+}
+
+void Queue::push(Commidity *elem)
+{
+    if (size == MAX_SIZE)
+    {
+        cout << "Queue is full " << endl;
+        return;
+    }
+
+    if (head == -1)
+    {
+        head = 0;
+    }
+
+    data[tail] = elem;
+    tail++;
+    size++;
+}
+
+Commidity *Queue::pop()
+{
+    if (head == -1)
+    {
+        cout << "Queue is empty " << endl;
+        return 0;
+    }
+    Commidity *temp = data[head];
+    for (int i = 0; i < size; i++)
+    {
+        data[i] = data[i + 1];
+    }
+    tail--;
+    size--;
+    if (size == 0)
+    {
+        head = -1;
+        tail = 0;
+    }
+    return temp;
+}
--- Queue.h
+++ Queue.h
@@ -1,27 +1,23 @@
 #pragma once
 
-template <typename T>
+#include "msg.h"
+
+#define MAX_SIZE 2048
+
+/* typedef char T; */
 
 class Queue
 {
-    T *data;
+    Commidity **data;
     int head;
     int tail;
-    int MAX_SIZE;
-    int current_size;
+    int size;
 
   public:
-    Queue(int);
-    bool is_empty();
-    int size();
-    void push(T);
-    void pop();
-    T front();
+    Queue();
+    int getSize();
+    bool isEmpty();
+    Commidity *front();
+    void push(Commidity *);
+    Commidity *pop();
 };
-/* custom_queue(int size) */
-/* { */
-/*     MAX_SIZE = size; */
-/*     this->data = new T[MAX_SIZE]; */
-/*     this->head = -1; */
-/*     this->tail = 0; */
-/*     this->size = 0; */
